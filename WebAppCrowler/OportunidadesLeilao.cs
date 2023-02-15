0f870f6eac81e459b615b374f1a223a50032dc38
fabiocostanegro <fabiocostanegro@gmail.com>
2023-02-15 08:07:19 -0300

    Criação da function de oportunidades de leilao

--- WebAppCrowler/OportunidadesLeilao.cs
+++ WebAppCrowler/OportunidadesLeilao.cs
@@ -7,33 +7,23 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using Fonte.Consultas.ConsultaValorJogador;
-using System.Collections.Generic;
-using Fonte.ConsultasWebApp.ConsultarValorJogador;
 
 namespace WebAppCrowler
 {
-    public static class ConsultaPreco
+    public static class OportunidadesLeilao
     {
-        [FunctionName("ConsultaPreco")]
+        [FunctionName("OportunidadesLeilao")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             string responseMessage = string.Empty;
-            
+
             if (req.Query["name"] == string.Empty)
                 return new BadRequestObjectResult("Parametros invalidos");
 
             string caminhoProfile = "user-data-dir=C:\\Users\\55319\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 3";
 
-            ConsultaValorJogadorWebApp consulta = new ConsultaValorJogadorWebApp(Fonte.FonteBase.Framework.Selenium, caminhoProfile, 30);
-            List<JogadorPrecoPrevisto> lista = new List<JogadorPrecoPrevisto>();
-            lista.Add(new JogadorPrecoPrevisto(req.Query["name"], Convert.ToInt32(req.Query["val"]), Convert.ToInt32(req.Query["inc"])));
-            List<JogadorValorMercadoAtual> valor =
-                consulta.ConsultarValorJogador(lista, 30);
-            responseMessage = valor[0].NomeJogador + " -> " + valor[0].ValorAtualMercado;
-            
             return new OkObjectResult(responseMessage);
         }
     }
